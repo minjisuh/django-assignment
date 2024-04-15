16e0fe1a4f2164e0f57b37dd723fd9186dcfc203
minjisuh <bt09037@gmail.com>
2024-04-16 02:08:13 +0900

    post

--- blog/models.py
+++ blog/models.py
@@ -11,8 +11,18 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     categories = models.ManyToManyField(Category, related_name='posts')
 
+
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    author = models.CharField(max_length=100)
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm, CommentForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(date_posted__lte=timezone.now()).order_by('date_posted')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -15,3 +16,16 @@ def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats)
     return render(request, 'blog/categories.html', {'cats': cats, 'category_posts': category_posts})
 
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
